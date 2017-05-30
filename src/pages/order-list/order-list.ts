85f563eec9a5a34d7ef54aac64fad53ee6e9254a
boneVIdy <120648590@qq.com>
2017-05-30 18:04:48 +0800

    #订单详情

--- src/pages/order-list/order-list.ts
+++ src/pages/order-list/order-list.ts
@@ -18,27 +18,5 @@ export class OrderListPage {
   curTab: string = "waitingForPay";
   constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
 
-  /**
-   * Perform a service for the proper items.
-   */
-  getItems(ev) {
-    let val = ev.target.value;
-    if (!val || !val.trim()) {
-      this.currentItems = [];
-      return;
-    }
-    this.currentItems = this.items.query({
-      name: val
-    });
-  }
-
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push(ItemDetailPage, {
-      item: item
-    });
-  }
 
 }
