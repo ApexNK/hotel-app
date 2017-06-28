eddbcc361e9d7ff1b19eedb023e0fa5953bb9893
hww <andyhuang@promote.cache-dns.local>
2017-06-28 15:27:18 +0800

    添加详情接口在支付界面

--- src/providers/API_MARCO.ts
+++ src/providers/API_MARCO.ts
@@ -2,7 +2,7 @@ export const LOGIN_KEY = 'mem-0004';   // 登录
 export const VALID_KEY = 'mem-0003';  // 获取验证码
 export const RECHARGE = 'money-czorder'; // 充值
 export const MINE = 'mem-myinfo'; // 个人信息
-
+export const ACCOUNT_DETAIL = 'mem-accountinfo';// 账号明细
 
 export const HOTEL_LIST = 'flat-0003';
 export const HOTEL_DETAIL = 'flat-0002';
