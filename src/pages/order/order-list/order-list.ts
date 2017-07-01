a9cfd7c50526286cdf92b32c0bbabac816f7a6fb
hww <andyhuang@promote.cache-dns.local>
2017-07-01 23:43:44 +0800

    优化订单更新

--- src/pages/order/order-list/order-list.ts
+++ src/pages/order/order-list/order-list.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Events } from 'ionic-angular';
 import {ORDER_STATE_ENUM, OrderManager, OrderItem} from '../../../providers';
 // import { ItemDetailPage } from '../hotel/room-detail/item-detail';
-
+import { ShowConfirmProvider } from '../../../providers/show-confirm/show-confirm';
 
 @Component({
   selector: 'order-list',
@@ -14,9 +14,16 @@ export class OrderListPage {
   public curTab = ORDER_STATE_ENUM.WAIT_PAY;
   public curPage = 1;
   public notLoadOver = true;
-  constructor(public navCtrl: NavController,
-              private orderManager: OrderManager,
-              navParams: NavParams) { }
+  constructor(public navCtrl: NavController, private orderManager: OrderManager,
+              navParams: NavParams,private events:Events, private confirmCtrl: ShowConfirmProvider) {
+    events.subscribe('updateOrder',(tabName)=>{
+      if(tabName && tabName !== this.curTab){
+        this.curTab = tabName;
+      }else{
+        this.getOrder();
+      }
+    })
+  }
   ionViewDidLoad() {
     this.getOrder();
     // this.getHotelList();
@@ -29,7 +36,17 @@ export class OrderListPage {
   public goPay (orderNo,index) {
     this.navCtrl.push('OrderPayPage',{orderNo});
   }
-  public async cancelOrder (ddbh, index) {
+
+  public cancelOrder(ddbh, index) {
+    this.confirmCtrl.show({message:"请确认是否申请退订",okText:"确定", cancelText:"取消"}).then(
+      result => {
+        if(result){
+          this.applyCancelOrder(ddbh,index);
+        }
+      });
+  }
+
+  public async applyCancelOrder (ddbh, index) {
     try {
       await this.orderManager.cancelOrder(ddbh);
       this.orderItems.splice(index,1);
@@ -37,7 +54,17 @@ export class OrderListPage {
       console.error(e);
     }
   }
-  public async leaveRoom (ddbh, index) {
+  public leaveRoom (ddbh, index) {
+    this.confirmCtrl.show({message:"请确认是否提交退房申请",okText:"确定", cancelText:"取消"}).then(
+      result => {
+        if(result){
+          this.applyToleave(ddbh,index);
+        }
+      }
+    );
+
+  }
+  private async applyToleave(ddbh, index) {
     try {
       await this.orderManager.leaveRoom(ddbh);
       this.orderItems.splice(index,1);
@@ -45,7 +72,9 @@ export class OrderListPage {
       console.error(e);
     }
   }
+
   public tabChange () {
+    console.info('tab changed');
     this.curPage = 1;
     this.orderItems = [];
     this.notLoadOver = true;
--- src/pages/order/order-pay/order-pay.ts
+++ src/pages/order/order-pay/order-pay.ts
@@ -1,9 +1,8 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { ShowConfirmProvider } from '../../../providers/show-confirm/show-confirm';
-import { ORDER_PAY, ORDER_DETAIL } from '../../../providers/API_MARCO';
+import { ORDER_PAY, ORDER_DETAIL, ORDER_STATE_ENUM } from '../../../providers/API_MARCO';
 import { WkDate } from '../../../util';
-
 /**
  * Generated class for the OrderPayPage page.
  *
@@ -30,7 +29,7 @@ export class OrderPayPage {
   public user = {name:'',phoneNumber:'',IDCard:''};
   public showSuccessPage = false;
   public amount: number;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private confirmCtrl: ShowConfirmProvider,@Inject('ApiService') api) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private events:Events, private confirmCtrl: ShowConfirmProvider,@Inject('ApiService') api) {
     this.api = api;
     this.days = 0;
     this.total = 0;
@@ -50,17 +49,16 @@ export class OrderPayPage {
         if( res.code === '0') {
           this.showSuccessPage = true;
           setTimeout(()=> {
-            this.goToOrderTabs();
+            this.goToOrderTabs(ORDER_STATE_ENUM.WAIT_USE);
           },2000);
 
         }else {
           this.confirmCtrl.show({message:"余额不足，不能满足付款",okText:"去充值", cancelText:"取消"}).then(
             result => {
               if(result){
-                console.info('go to rechange page');
                 this.navCtrl.push("RechargePage",{payWay:'aliPay'});
               } else {
-                this.goToOrderTabs();
+                this.goToOrderTabs(ORDER_STATE_ENUM.WAIT_PAY);
               }
             }
           );
@@ -73,9 +71,10 @@ export class OrderPayPage {
 
 }
 
-  private goToOrderTabs(){
+  private goToOrderTabs(tab){
     this.navCtrl.parent.select(1);
     this.navCtrl.popToRoot();
+    this.events.publish('updateOrder',tab);
   }
 
 /*  private goToKeyTabs() {
