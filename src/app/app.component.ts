92c7283a1e44689eb52403d72e99b12bf8f4584f
hww <andyhuang@promote.cache-dns.local>
2017-07-09 01:04:28 +0800

    两次回退退出应用，其它标签回退到首页

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav, Config } from 'ionic-angular';
+import { Platform, Nav, Config , App, Keyboard } from 'ionic-angular';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -8,7 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import {TabsPage as FirstRunPage } from '../pages/tabs/tabs';
 // import { LoginPage as FirstRunPage } from '../pages/user/login/login';
 
-import { Settings } from '../providers/providers';
+import { Settings, Toast } from '../providers';
 
 
 
@@ -22,16 +22,74 @@ export class MyApp {
   rootPage = FirstRunPage;
 
   @ViewChild(Nav) nav: Nav;
-  constructor(platform: Platform, settings: Settings, private config: Config, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform, settings: Settings, private config: Config, statusBar: StatusBar, splashScreen: SplashScreen,
+              private app: App, private keyboard: Keyboard,private toast:Toast) {
 
-    platform.ready().then(() => {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       setTimeout(() => {
         splashScreen.hide();
       }, 300);
-
+      this.registerBackButtonAction();//注册物理回退事件
     });
+
+  }
+
+  private registerBackButtonAction() {
+    this.platform.registerBackButtonAction(() => {
+      debugger;
+
+      if (this.keyboard.isOpen()) {
+        this.keyboard.close();
+        return;
+      }
+
+      const overlay = this.app._appRoot._overlayPortal.getActive();
+      if(overlay && overlay.dismiss) {
+        overlay.dismiss();
+        return;
+      }
+
+      const nav = this.app.getActiveNav();
+      let activeVC = nav.getActive();
+      let page = activeVC.instance;
+
+      console.log(page);
+      debugger;
+
+      //if (page instanceof IonTabsPage) {
+       // this.app.goBack();
+       // return;
+     // }
+
+      if(nav.canGoBack()){
+        nav.pop();
+      } else {
+        if(nav.id === 't0-0'){//在首页
+          this.showExit();
+        }　else {
+          //返回首页
+          //this.app.goBack();
+          nav.parent.select(0);
+        }
+
+      }
+    });
+  }
+
+//双击退出提示框
+  private backButtonPressed: boolean = false;
+  showExit() {
+    if (this.backButtonPressed) { //当触发标志为true时，即2秒内双击返回按键则退出APP
+      this.platform.exitApp();
+    } else {
+      this.toast.show('再按一次退出应用');
+      this.backButtonPressed = true;
+      setTimeout(() => { //2秒内没有再次点击返回则将触发标志标记为false
+        this.backButtonPressed = false;
+      }, 2000)
+    }
   }
 }
