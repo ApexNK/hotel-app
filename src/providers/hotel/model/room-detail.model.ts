d335d745ac519d5193ddc9f775144c96ddca7edf
hww <andyhuang@promote.cache-dns.local>
2017-07-10 22:18:04 +0800

    房间详情

--- src/providers/hotel/model/room-detail.model.ts
+++ src/providers/hotel/model/room-detail.model.ts
@@ -1,9 +1,9 @@
 export interface RoomDetail {
-  fjmc: string;
-  fjjg: number;
-  fjmj: number;
-  chcx: string;
-  sslc: number;
-  fjdz: string;
-  fjtp: { Tpmc: string; Tplj: string;}[]
+  fjmc: string;//房间名称
+  fjjg: number;//房间价格
+  fjmj: number;//房间面积
+  chcx: string;//房间朝向
+  sslc: number;//所属楼层
+  fjdz: string;//房间地址
+  fjtp: { imgName: string; imgPath: string;}[]//房间图片
 }
