93b5477b654fe6c0e1f1a3a918f128588a353506
jinggk <jingguokai@youbangsoft.com>
2017-07-14 11:04:37 +0800

    # 地图问题合入

--- src/pages/hotel/room-list/room-list.ts
+++ src/pages/hotel/room-list/room-list.ts
@@ -1,48 +1,54 @@
-import { Component , ViewChild, ElementRef} from '@angular/core';
-import { NavController, NavParams ,IonicPage} from 'ionic-angular';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController, NavParams, IonicPage, ActionSheetController, Platform} from 'ionic-angular';
 import {HotelDetail, HotelManager} from '../../../providers';
-import { HotelMap } from "../../../models/hotelmap";
+import {HotelMap} from "../../../models/hotelmap";
+declare const window: Window;
 
 @IonicPage({
-  name:"RoomListPage",
+  name: "RoomListPage",
   segment: 'RoomListPage/:flatId/:beginDate/:endDate'
 })
 @Component({
   selector: 'room-list',
   templateUrl: 'room-list.html'
 })
-export class RoomListPage{
+export class RoomListPage {
   item: any;
   public beginDate = '';
   public endDate = '';
   public id = '';
   public hotelDetail: HotelDetail;
   @ViewChild('hotelmap') mapElement: ElementRef;
+
   constructor(public navCtrl: NavController,
-              private hotelManager:  HotelManager,
+              private hotelManager: HotelManager,
+              public platform: Platform,
+              private actionSheetCtrl: ActionSheetController,
               navParams: NavParams) {
     this.id = navParams.get('flatId');
     this.beginDate = navParams.get('beginDate');
     this.endDate = navParams.get('endDate');
   }
+
   ionViewDidLoad() {
     this.getHotelDetail();
     // this.getHotelList();
   }
 
-  public getHotelDetail () {
+  public getHotelDetail() {
     this.hotelManager.getHotelDetail({beginDate: this.beginDate, endDate: this.endDate, flatId: this.id})
       .then((res) => {
         this.hotelDetail = res as HotelDetail;
-        try{
-          this.showMap({longitude:this.hotelDetail.longitude,latitude: this.hotelDetail.latitude});
-        }catch (err){
-           console.info(err);
+        try {
+          this.showMap({longitude: this.hotelDetail.longitude, latitude: this.hotelDetail.latitude});
+        } catch (err) {
+          console.info(err);
         }
 
       });
   }
-  public goRoomDetail (roomId, beginDate, endDate, fjbh) {
+
+  public goRoomDetail(roomId, beginDate, endDate, fjbh) {
     this.navCtrl.push("ItemDetailPage", {
       roomId,
       beginDate,
@@ -50,16 +56,43 @@ export class RoomListPage{
       fjbh
     });
   }
+
   public openBaiDuMap() {
-    let target = 'bdapp://map/geocoder?location=' + this.hotelDetail.latitude + ',' + this.hotelDetail.longitude;
-    console.log(target);
-    window.location.href = target;
+    let baiduTarget = 'bdapp://map/geocoder?location=' + this.hotelDetail.latitude + ',' + this.hotelDetail.longitude;
+
+    let actionSheet = this.actionSheetCtrl.create({
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: '百度地图',
+          role: 'destructive',
+          handler: () => {
+            console.log(baiduTarget);
+            window.open(baiduTarget);
+          }
+        },
+        {
+          text: '高德地图',
+          handler: () => {
+            alert('暂不支持高德地图');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
-  private showMap (location:any) {
+  private showMap(location: any) {
     let map = new HotelMap(this.mapElement.nativeElement);
     let long = location.longitude;
     let lati = location.latitude;
-    map.createHotelNearbyMap(long,lati);
+    map.createHotelNearbyMap(long, lati);
   }
 }
