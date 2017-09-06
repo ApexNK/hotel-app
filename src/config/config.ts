83acc698f5b397584fb01b8be996151f558c625f
hww <andyhuang@promote.cache-dns.local>
2017-09-06 21:23:21 +0800

    fix alipay ios issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,9 @@ const isDev: boolean = false;
 const devUrl: string = 'http://wkzj.tunnel.echomod.cn/hostel-app-war/app/serviceApi';
 const proUrl: string = 'http://121.196.194.174/hostel-app-war/app/serviceApi';
 // const proUrl: string = 'http://wkzj.tunnel.echomod.cn/hostel-app-war/app/serviceApi';
-const imgServerUrl: string = 'http://121.196.194.174:8086/'; // 读取图片路径
+// const imgServerUrl: string = 'http://121.196.194.174:8086/'; // 读取图片路径
+const imgServerUrl: string = 'http://121.196.194.174/hostel-admin-war/admin/image/view/'; // 读取图片路径
+//const imgServerUrl: string = 'http://wkzj.tunnel.echomod.cn/hostel-admin-war/admin/image/view/'; // 读取图片路径
 const apkDownloadUrl: string = 'http://121.196.194.174/hostel-app-war/upgrade/stable'; // 包下载
 const imgAPI = 'http://121.196.194.174/hostel-app-war/app/appUploadImg';
 
